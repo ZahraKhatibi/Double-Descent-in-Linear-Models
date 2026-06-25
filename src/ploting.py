2b0d64d6c3b804da0d0648899c99eef994fd190d
Zahra Khatibi <zahrakhatibiiii@gmail.com>
2026-06-25 18:26:05 +0200

    adding ploting file

---
+++ src/ploting.py
@@ -0,0 +1,6 @@
+import os
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+sys.path.insert(0, os.getcwd())
+from src.experiments import run_experiment, run_noise_sweep, run_gd_experiment
